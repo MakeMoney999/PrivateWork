6a6bc0a2968f03a1495bcc61d7d8c9d95975dd65
wangyang <yang.wang@zenjoy.net>
2021-10-31 21:34:08 +0800

    提交一下

--- WeChat_Scrap/run.py
+++ WeChat_Scrap/run.py
@@ -26,6 +26,7 @@ def main():
 	ip=get_host_ip()
 	print ("代理服务器已启动")
 	print ("请配置wifi代理模式,ip:"+ip,"端口号:"+port)
+	print ("配置完成后访问mitm.it页面下载证书")
 	print ("配置完成后请打开浏览器访问www.jcgame.net进行链接测试")
 	run_mitmproxy(command)
 	
--- WeChat_Scrap/wechat_login.py
+++ WeChat_Scrap/wechat_login.py
@@ -2,43 +2,42 @@ from mitmproxy.http import flow
 import mitmproxy
 import json
 import os
-import socket
-import time
-import subprocess
 
 
-
-
-
+class check_connect():
+    """检查链接状态"""
+    def request(self,flow):
+        #print (flow.request.url)
+        if "www.jcgame.net" in flow.request.url:
+            print("连接成功")
 
 
 class wechat_app_login():
 
-    def client_connected(self, client: mitmproxy.connection.Client):
-        print ("客户端链接成功")
-        a=1
-        return a 
 
     def __init__(self,domain):
         self.domain=domain 
         self.url=domain+"/oauth/v2/wechat/mini-program/authorization-code/login"
 
     def request(self,flow):
-        #print (self.url)
+        #print (flow.request.url)
         if self.url in flow.request.url:
             requestbody=flow.request.get_content()
             requestbody=json.loads(requestbody)
             flow.request.set_text(json.dumps(requestbody))
-            print(flow.request.get_text())
+            #print(flow.request.get_text())
 
     def response(self,flow):
         if self.url in flow.request.url:
-            print (flow.response.content)
+            #print (flow.response.text)
             if flow.response.status_code == 200:
-                res=flow.response.content  #login的返回
-                start_Scrape(res[])#开始执行爬虫相关数据
+                res=json.loads(flow.response.text)  #login的返回
+                access_token=res["access_token"]
+                print (access_token)
+                start_Scrape(access_token)#开始执行爬虫相关程序##############从这里写
 
         
 addons = [
+    check_connect(),
     wechat_app_login("https://oauth.marykayintouch.com.cn")
 ]
